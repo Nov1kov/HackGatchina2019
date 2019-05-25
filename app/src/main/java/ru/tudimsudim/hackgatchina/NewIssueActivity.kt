39fb8463a716c6cd32185a472446dae342a6c1f7
aliascage <g.triquivel@gmail.com>
2019-05-26 01:17:42 +0300

    Close activity after commit

--- app/src/main/java/ru/tudimsudim/hackgatchina/NewIssueActivity.kt
+++ app/src/main/java/ru/tudimsudim/hackgatchina/NewIssueActivity.kt
@@ -54,6 +54,8 @@ class NewIssueActivity : AppCompatActivity() {
                 ex.printStackTrace()
             }
         }
+        Toast.makeText(this, "Отправлена проблема", Toast.LENGTH_LONG).show()
+        finish()
     }
 
     val REQUEST_IMAGE_CAPTURE = 1
